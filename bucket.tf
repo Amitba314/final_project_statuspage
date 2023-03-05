84d2d463c0955f2cec9499f34261e812e66d7f40
Yaringabay1 <117725330+Yaringabay1@users.noreply.github.com>
2023-03-05 18:57:33 +0200

    Delete bucket.tf

--- bucket.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "only-gabay-project"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    access_key     = "AKIAVDQNLGLNKUZUVOSJ"
-    secret_key     = "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"
-  }
-}
