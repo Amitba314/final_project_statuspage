9cd4af3315b87340fac69a267f45fad2ab7c1c06
Yaringabay1 <117725330+Yaringabay1@users.noreply.github.com>
2023-03-05 19:01:33 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_id" {
-  description = "Name of the EKS cluster where the ingress nginx will be deployed"
-  type        = string
-}
-
-variable "update_image_tag" {
-  description = "The actual new image tag for the new image base on commit id"
-  type        = string
-}
