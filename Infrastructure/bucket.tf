7fc1d26982a12fc32775d412c1e2106a64236327
Amitba314 <117725696+Amitba314@users.noreply.github.com>
2023-03-07 00:12:36 +0200

    Update bucket.tf

--- Infrastructure/bucket.tf
+++ Infrastructure/bucket.tf
@@ -3,8 +3,9 @@ terraform {
     bucket         = "statuspage-s3"
     key            = "terraform.tfstate"
     region         = "us-east-2"
-    access_key     = ${{ secrets.AMIT_ACCESS_KEY_ID }}
-    secret_key     = ${{ secrets.AMIT_SECRET_ACCESS_KEY }}
+    access_key     = "AKIAX7K52CAD6NPNZBWI"
+    secret_key     = "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
+"
   }
 
 }
