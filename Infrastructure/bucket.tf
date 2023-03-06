794466eac34e8a73bee7907f99f4bc43ac50ab1b
Amitba314 <117725696+Amitba314@users.noreply.github.com>
2023-03-07 00:15:26 +0200

    Update bucket.tf

--- Infrastructure/bucket.tf
+++ Infrastructure/bucket.tf
@@ -4,8 +4,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-2"
     access_key     = "AKIAX7K52CAD6NPNZBWI"
-    secret_key     = "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
-"
+    secret_key     = "P6CKLYS/BGGZt1bS8XHAMkndsqLnV5Qcl1ZfrhZv"
   }
-
 }
