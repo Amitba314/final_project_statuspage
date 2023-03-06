5441c6f50f02e9cde5727c2cc37168b92fef4429
Yaringabay1 <117725330+Yaringabay1@users.noreply.github.com>
2023-03-06 21:30:13 +0200

    Update full_eks_module.tf

--- Infrastructure/full_eks_module.tf
+++ Infrastructure/full_eks_module.tf
@@ -4,11 +4,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.12.0"
 
-  name = "amit-eks-vpc"
+  name = "amit-eks-vpc-2.0"
 
   cidr = "10.0.0.0/16" # Replace with your desired CIDR block
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"] # Replace with your desired AZs
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"] # Replace with your desired AZs
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] # Replace with your desired private subnets
   public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"] # Replace with your desired public subnets
 
